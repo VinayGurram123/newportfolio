9410e7cae65433b9d6a689c08a267ba54f0ba13b
VinayGurram123 <vinaygurram143@gmail.com>
2024-09-09 12:36:05 +0530

    automatic

--- git.sh
+++ git.sh
@@ -1,16 +1,16 @@
-" adding files and changes"
+echo " adding files and changes"
 
 git add .
 
-"commenting all the changes"
+echo "commenting all the changes"
 
 git commit -m "automatic"
 
 
-"pushing files to master"
+echo "pushing files to master"
 
 git push origin master
 
 
-"successfully done"
+echo "successfully done"
 
